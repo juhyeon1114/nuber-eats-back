6a7281c9249e9a3b313239f4017cd0de97ea140e
unknown <juhyeon1114@gmail.com>
2021-02-05 21:45:49 +0900

    log : validate Args by using class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // validation decorator가 없으면 아예 거름
+      forbidNonWhitelisted: true, // validation decorator가 없는 값이 들어오면 error뱉음
+      transform: true, // request를 우리가 원하는 실제 타입으로 변환해줌
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,15 +1,24 @@
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 // @InputType()
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
+
   @Field(() => String)
+  @IsString()
   address: string;
+
   @Field(() => String)
+  @IsString()
   ownerName: string;
 }
 
