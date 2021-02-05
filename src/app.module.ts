e901595be93989d15eb43902195b109f784034dc
unknown <juhyeon1114@gmail.com>
2021-02-05 20:54:41 +0900

    log : first schema, query

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+// import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      // autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 스키마 파일을 자동으로 생성
+      autoSchemaFile: true, // 스키마를 자동으로 생성해서 메모리에 갖고 있음
+    }),
+    RestaurantsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
