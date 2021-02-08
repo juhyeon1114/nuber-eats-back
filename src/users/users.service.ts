11d18a6422add8edabc8df00ca3b9f1e119c332f
unknown <juhyeon1114@gmail.com>
2021-02-08 21:29:27 +0900

    log : make the function creates a Account

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,4 +10,25 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check new user
+    // ? create user & hash the password
+    // : return something
+
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        return 'There is a user with that email already';
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+    } catch (error) {
+      console.error(error);
+      return 'Could not create account';
+    }
+  }
 }
