c93b06c8587c1b9e90f4fcd4bd802bad18b4ac1b
unknown <juhyeon1114@gmail.com>
2021-02-10 16:02:49 +0900

    log : detail

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -14,14 +14,14 @@ export class JwtMiddleware implements NestMiddleware {
     if ('x-jwt' in req.headers) {
       // console.log(req.headers['x-jwt']);
       const token = req.headers['x-jwt'];
-      const decoded = this.jwtService.verify(token.toString());
-      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
-        try {
+      try {
+        const decoded = this.jwtService.verify(token.toString());
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
           const user = await this.usersService.findById(decoded['id']);
           req['user'] = user;
-        } catch (error) {
-          console.error(error);
         }
+      } catch (error) {
+        console.error(error);
       }
     }
     next();
