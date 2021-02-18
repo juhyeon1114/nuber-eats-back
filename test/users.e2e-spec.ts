e13eedf4af47be3a9ba2dfd2c6b4d1a0ab67c139
unknown <juhyeon1114@gmail.com>
2021-02-18 17:45:39 +0900

    log : complete e2e testing about Login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,8 +4,20 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+// 테스트마다 email을 전송하지 않게 하기 위함
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+const EMAIL = 'juhyeon@gomiad.com';
+const PASSWORD = 'rlawngus';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let jwtToken: string;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +34,99 @@ describe('UserModule (e2e)', () => {
     await app.close(); // app종료 (required)
   });
 
-  it.todo('createAccount');
+  // common functions
+  const createTestAccount = (email, password) => {
+    return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+        mutation {
+          createAccount(input: {
+            email: "${email}",
+            password: "${password}",
+            role: Client
+          }) {
+            ok
+            error
+          }
+        }
+      `,
+      });
+  };
+
+  const login = (email, password) => {
+    return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+      mutation {
+        login(input: {
+          email: "${email}",
+          password: "${password}"
+        }) {
+          ok
+          error
+          token
+        }
+      }
+    `,
+      });
+  };
+
+  describe('createAccount', () => {
+    it('should create account', () => {
+      return createTestAccount(EMAIL, PASSWORD)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', async () => {
+      return createTestAccount(EMAIL, PASSWORD)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return login(EMAIL, PASSWORD)
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+
+    it('should not be able to login with wrong credentials', () => {
+      return login(EMAIL, 'wrongPassword')
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toBe(null);
+        });
+    });
+  });
+
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
